5ed10984504a862d24dfcbf0f27bc53ce938d58a
Huanhuan Sun <husun@microsoft.com>
2017-01-12 17:15:03 -0800

    Initial commit

---
+++ C#/ContosoAdsWebJob/Functions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using ContosoAdsCommon;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+
+namespace ContosoAdsWebJob
+{
+    public class Functions
+    {
+        public static void GenerateThumbnail(
+        [QueueTrigger("thumbnailrequest")] BlobInformation blobInfo,
+        [Blob("images/{BlobName}", FileAccess.Read)] Stream input,
+        [Blob("images/{BlobNameWithoutExtension}_thumbnail.jpg")] CloudBlockBlob outputBlob)
+        {
+            using (Stream output = outputBlob.OpenWrite())
+            {
+                ConvertImageToThumbnailJPG(input, output);
+                outputBlob.Properties.ContentType = "image/jpeg";
+            }
+
+            // Entity Framework context class is not thread-safe, so it must
+            // be instantiated and disposed within the function.
+            // Open storage account using credentials from .cscfg file.
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ToString());
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("ads");
+            table.CreateIfNotExists();
+
+            var id = blobInfo.AdId;
+            Ad ad = FindAdFromId(table, id);
+            if (ad == null)
+            {
+                throw new Exception(String.Format("AdId {0} not found, can't create thumbnail", id.ToString()));
+            }
+            ad.ThumbnailURL = outputBlob.Uri.ToString();
+            table.Execute(TableOperation.Replace(ad.ToAd()));
+        }
+
+        public static void ConvertImageToThumbnailJPG(Stream input, Stream output)
+        {
+            int thumbnailsize = 80;
+            int width;
+            int height;
+            var originalImage = new Bitmap(input);
+
+            if (originalImage.Width > originalImage.Height)
+            {
+                width = thumbnailsize;
+                height = thumbnailsize * originalImage.Height / originalImage.Width;
+            }
+            else
+            {
+                height = thumbnailsize;
+                width = thumbnailsize * originalImage.Width / originalImage.Height;
+            }
+
+            Bitmap thumbnailImage = null;
+            try
+            {
+                thumbnailImage = new Bitmap(width, height);
+
+                using (Graphics graphics = Graphics.FromImage(thumbnailImage))
+                {
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    graphics.DrawImage(originalImage, 0, 0, width, height);
+                }
+
+                thumbnailImage.Save(output, ImageFormat.Jpeg);
+            }
+            finally
+            {
+                if (thumbnailImage != null)
+                {
+                    thumbnailImage.Dispose();
+                }
+            }
+        }
+
+        private static Ad FindAdFromId(CloudTable table, int id)
+        {
+            return table.ExecuteQuery(new TableQuery<Ad>().Where(TableQuery.GenerateFilterConditionForInt("AdId", QueryComparisons.Equal, id))).FirstOrDefault();
+        }
+    }
+}
